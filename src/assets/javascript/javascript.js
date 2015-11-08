ccc3e25b1f1cde645c5f318845a379559172a9c6
YaronMiro <yaron@gizra.com>
2015-11-08 09:30:58 +0200

    wip: adding js to alter iframe to march a breakpoint

---
+++ src/assets/javascript/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  /**
+   * Change the width of the "iframe" to match a breakpoint range.
+   */
+  var changeBreakPoint = function() {
+    // The class to add to the iframe.
+    var _class = $(this).data("breakpoint");
+
+    // Alter the "iframe" class.
+    var $iframe = $("iframe");
+    $iframe.removeClass();
+    $iframe.addClass(_class);
+  };
+
+  // The on "click" handler.
+  $("#main-navigation li.device").on("click", changeBreakPoint);
+
+});
