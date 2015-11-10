bde99525556310aac9c3e00431718f33e218fb7a
YaronMiro <yaron@gizra.com>
2015-11-10 10:44:04 +0200

    Adding window width change js logic

--- src/assets/javascript/javascript.js
+++ src/assets/javascript/javascript.js
@@ -1,23 +1,51 @@
 $(document).ready(function() {
 
+  // The last window "width" value.
+  var prevW = $(window).width();
+
+
+  function clearClass(className) {
+    // Strip the "active" class.
+    $('#breakpoints-navigation li.device').each(function() {
+      $(this).removeClass(className);
+    });
+  }
+
+  /**
+   * Clear the "iframe" breakpoint class (e.g "xs") and the "active" class from
+   * the icon that's responsible to display the current breakpoint.
+   */
+  $(window).resize(function() {
+
+    // In case we change the height only.
+    if ($(window).width() == prevW) {
+      return;
+    }
+
+    // Update the new window "width" value.
+    prevW = $(window).width();
+
+    // Strip class from the breakpoints icons.
+    clearClass('active');
+
+  });
+
   /**
    * Change the width of the "iframe" to match a breakpoint range.
    */
   var changeBreakPoint = function() {
-    // The class to add to the iframe.
-    var _class = $(this).data("breakpoint");
 
-    // Strip class.
-    $("#breakpoints-navigation li.device").each(function() {
-      $(this).removeClass();
-    });
+    var self = this;
 
-    $(this).addClass('active-' + _class);
+    // Strip class from the breakpoints icons.
+    clearClass('active');
+
+    $(self).addClass('active');
 
     // Alter the "iframe" class.
     var $iframe = $("iframe");
     $iframe.removeClass();
-    $iframe.addClass(_class);
+    $iframe.addClass($(self).data("breakpoint"));
   };
 
   // The on "click" handler.
